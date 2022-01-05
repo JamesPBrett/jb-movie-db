bd6d7ddbf19715ace20bee8ac820bb07f98f2004
james brett <james@jamesbrett.co.uk>
2022-01-05 19:29:24 +0000

    Movie DB - Search Form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,7 +2,19 @@ import React from "react";
 import { useGlobalContext } from "../contexts/context";
 
 const SearchForm = () => {
-  return <h2>search component</h2>;
+  const { query, setQuery, error } = useGlobalContext();
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>Search movies</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {error.show && <div className="error">{error.msg}</div>}
+    </form>
+  );
 };
 
 export default SearchForm;
